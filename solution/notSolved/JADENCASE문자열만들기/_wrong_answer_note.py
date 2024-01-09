bf451e5c83ad44d5f6d0dbaee5de2e8f4fa9b456
ghghwldk <ghghwldk@gmail.com>
2024-01-09 23:52:41 +0900

    wrong answer not added.

---
+++ solution/notSolved/JADENCASE문자열만들기/_wrong_answer_note.py
@@ -0,0 +1,22 @@
+'''
+[Reason for wrong answer]
+I didn't check the empty string for the splitted strings.
+
+The problem provides me the contraints to follow.
+I have to consider the consecutive space in the string.
+
+Consecutive space makes the empty strings after splitting string.
+
+[Ability problem]
+It is not the mistake. It is from my own scarce ability.
+To prevent the same issue.
+I have to recognize the wrong answer is from my scarce ability.
+
+[strategy for the future]
+Not to make the problem from not-following the constraints.
+
+I've to read over the constraints and the conditions from the problem.
+
+I've to do this before submitting my answer.
+It is the optimal time to check
+'''
