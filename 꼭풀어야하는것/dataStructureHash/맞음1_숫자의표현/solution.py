cfd051731ff54642faf3260f0083ff7b2b1f2019
ghghwldk <ghghwldk@gmail.com>
2024-05-06 14:39:07 +0900

    숫자의 표현 정리

--- 꼭풀어야하는것/dataStructureHash/맞음1_숫자의표현/solution.py
+++ 꼭풀어야하는것/dataStructureHash/맞음1_숫자의표현/solution.py
@@ -9,8 +9,16 @@ def solution(n):
     result = 0
     for k in sorted(h.keys(), reverse = True):
         target = k - n
-        if target in h.keys():
+
+        # if target in h.keys():
+        #     result += 1
+        '''in h로 작성해야 시간복잡도가 1이다.'''
+        if target in h:
             result += 1
         if k < n:
             break
     return result
+
+
+n = 15
+print(solution(n))
